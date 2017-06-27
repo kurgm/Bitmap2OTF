57a68afb55c025cd7b61ed7cf42f8709ee17deee
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-06-27 21:53:49 +0900

    autopep8

--- bitmap2otf/bitmap.py
+++ bitmap2otf/bitmap.py
@@ -2,27 +2,27 @@
 
 
 def _makebold_row_type0(row, align=False):
-	row0 = row + [0]
-	row1 = [0] + row
-	return [b0 or b1 for b0, b1 in zip(row0, row1)]
+    row0 = row + [0]
+    row1 = [0] + row
+    return [b0 or b1 for b0, b1 in zip(row0, row1)]
 
 
 def _makebold_row_type1(row, alignRight=False):
-	if alignRight:
-		row0 = [0] + row
-		row1 = row + [0]
-		row2 = row[1:] + [0, 0]
-	else:
-		row0 = row + [0]
-		row1 = [0] + row
-		row2 = [0, 0] + row[:-1]
+    if alignRight:
+        row0 = [0] + row
+        row1 = row + [0]
+        row2 = row[1:] + [0, 0]
+    else:
+        row0 = row + [0]
+        row1 = [0] + row
+        row2 = [0, 0] + row[:-1]
 
-	return [b1 or (b0 and not b2) for b0, b1, b2 in zip(row0, row1, row2)]
+    return [b1 or (b0 and not b2) for b0, b1, b2 in zip(row0, row1, row2)]
 
 
 _makebold_row = {
-	0: _makebold_row_type0,
-	1: _makebold_row_type1
+    0: _makebold_row_type0,
+    1: _makebold_row_type1
 }
 
 #    U
@@ -36,248 +36,256 @@ _R = 2
 _U = 3
 
 _polygons_bit2edgedirs = {
-	(False, False, False, False): (),
-	(False, False, False, True ): ((_U, _R), ),
-	(False, False, True , False): ((_L, _U), ),
-	(False, False, True , True ): (),
-	(False, True , False, False): ((_R, _D), ),
-	(False, True , False, True ): (),
-	(False, True , True , False): ((_R, _D), (_L, _U)),
-	(False, True , True , True ): ((_L, _D), ),
-	(True , False, False, False): ((_D, _L), ),
-	(True , False, False, True ): ((_D, _L), (_U, _R)),
-	(True , False, True , False): (),
-	(True , False, True , True ): ((_D, _R), ),
-	(True , True , False, False): (),
-	(True , True , False, True ): ((_U, _L), ),
-	(True , True , True , False): ((_R, _U), ),
-	(True , True , True , True ): ()
+    (False, False, False, False): (),
+    (False, False, False, True): ((_U, _R), ),
+    (False, False, True, False): ((_L, _U), ),
+    (False, False, True, True): (),
+    (False, True, False, False): ((_R, _D), ),
+    (False, True, False, True): (),
+    (False, True, True, False): ((_R, _D), (_L, _U)),
+    (False, True, True, True): ((_L, _D), ),
+    (True, False, False, False): ((_D, _L), ),
+    (True, False, False, True): ((_D, _L), (_U, _R)),
+    (True, False, True, False): (),
+    (True, False, True, True): ((_D, _R), ),
+    (True, True, False, False): (),
+    (True, True, False, True): ((_U, _L), ),
+    (True, True, True, False): ((_R, _U), ),
+    (True, True, True, True): ()
 }
 
 
 def _binary2imagedata(bitarray):
-	bytes = ""
-	for i in range(0, len(bitarray), 8):
-		byteArray = bitarray[i:i + 8]
-		if len(byteArray) < 8:
-			byteArray += [False] * (8 - len(byteArray))
-		byte = 0
-		for i in range(8):
-			byte <<= 1
-			if byteArray[i]:
-				byte |= 1
-		bytes += chr(byte)
+    bytes = ""
+    for i in range(0, len(bitarray), 8):
+        byteArray = bitarray[i:i + 8]
+        if len(byteArray) < 8:
+            byteArray += [False] * (8 - len(byteArray))
+        byte = 0
+        for i in range(8):
+            byte <<= 1
+            if byteArray[i]:
+                byte |= 1
+        bytes += chr(byte)
 
-	return bytes
+    return bytes
 
 
 class Bitmap(object):
-	"""Bitmap with metrics. Bottom-to-top and left-to-right."""
-	def __init__(self, bitmap=[[]], origin=(0, 0), advance=None, voriginy=0):
-		self.bitmap = [row[:] for row in bitmap]  # copy
-		self.height = len(bitmap)
-		self.width = len(bitmap[0])
-		self.origin = origin
-		if advance is None:
-			advance = (self.width, self.height)
-		self.advanceWidth, self.advanceHeight = advance
-		self.voriginy = voriginy
-		for r in bitmap:
-			assert len(r) == self.width
+    """Bitmap with metrics. Bottom-to-top and left-to-right."""
 
-	def makebold(self, boldtype=0, x=1, y=0, x2=0, y2=0):
-		for i in range(x):
-			self._makebold_x(boldtype, i < x2)
+    def __init__(self, bitmap=[[]], origin=(0, 0), advance=None, voriginy=0):
+        self.bitmap = [row[:] for row in bitmap]  # copy
+        self.height = len(bitmap)
+        self.width = len(bitmap[0])
+        self.origin = origin
+        if advance is None:
+            advance = (self.width, self.height)
+        self.advanceWidth, self.advanceHeight = advance
+        self.voriginy = voriginy
+        for r in bitmap:
+            assert len(r) == self.width
 
-		self.bitmap = [list(row) for row in zip(*self.bitmap)]
+    def makebold(self, boldtype=0, x=1, y=0, x2=0, y2=0):
+        for i in range(x):
+            self._makebold_x(boldtype, i < x2)
 
-		for i in range(y):
-			self._makebold_x(boldtype, i < y2)
+        self.bitmap = [list(row) for row in zip(*self.bitmap)]
 
-		self.bitmap = [list(row) for row in zip(*self.bitmap)]
+        for i in range(y):
+            self._makebold_x(boldtype, i < y2)
 
-		self.width += x
-		self.height += y
-		self.advanceWidth += x
-		self.advanceHeight += y
-		self.voriginy += y
+        self.bitmap = [list(row) for row in zip(*self.bitmap)]
 
-	def _makebold_x(self, boldtype=0, align=False):
-		self.bitmap = [_makebold_row[boldtype](row, align) for row in self.bitmap]
+        self.width += x
+        self.height += y
+        self.advanceWidth += x
+        self.advanceHeight += y
+        self.voriginy += y
 
-	def makeitalic(self, cotangent):
-		if cotangent == 0:
-			return
+    def _makebold_x(self, boldtype=0, align=False):
+        self.bitmap = [_makebold_row[boldtype](
+            row, align) for row in self.bitmap]
 
-		slanttoleft = cotangent < 0
-		abscotangent = abs(cotangent)
+    def makeitalic(self, cotangent):
+        if cotangent == 0:
+            return
 
-		y = 0
-		while y < self.height:
-			for i in range(self.height):
-				if (i < y) ^ slanttoleft:
-					self.bitmap[i].append(0)
-				else:
-					self.bitmap[i].insert(0, 0)
-			y += abscotangent
+        slanttoleft = cotangent < 0
+        abscotangent = abs(cotangent)
 
-		ox, oy = self.origin
-		ox += oy / float(cotangent)
+        y = 0
+        while y < self.height:
+            for i in range(self.height):
+                if (i < y) ^ slanttoleft:
+                    self.bitmap[i].append(0)
+                else:
+                    self.bitmap[i].insert(0, 0)
+            y += abscotangent
 
-		self.width = len(self.bitmap[0])
-		self.origin = (ox, oy)
+        ox, oy = self.origin
+        ox += oy / float(cotangent)
 
-	def translate(self, x=0, y=0):
-		ox, oy = self.origin
-		self.origin = (ox - x, oy - y)
+        self.width = len(self.bitmap[0])
+        self.origin = (ox, oy)
 
-	def rotate(self, n=1):
-		assert isinstance(n, int)
+    def translate(self, x=0, y=0):
+        ox, oy = self.origin
+        self.origin = (ox - x, oy - y)
 
-		n %= 4
+    def rotate(self, n=1):
+        assert isinstance(n, int)
 
-		if n == 0:
-			return
-		if n == 2:
-			self.bitmap.reverse()
-			for r in self.bitmap:
-				r.reverse()
-			self.origin = (self.width - self.origin[0], self.height - self.origin[1])
-			return
+        n %= 4
 
-		if n == 1:
-			self.bitmap = [list(row) for row in zip(*self.bitmap)]
-			self.bitmap.reverse()
-			self.origin = (self.origin[1], self.width - self.origin[0])
-		if n == 3:
-			self.bitmap.reverse()
-			self.bitmap = [list(row) for row in zip(*self.bitmap)]
-			self.origin = (self.height - self.origin[1], self.origin[0])
+        if n == 0:
+            return
+        if n == 2:
+            self.bitmap.reverse()
+            for r in self.bitmap:
+                r.reverse()
+            self.origin = (
+                self.width - self.origin[0], self.height - self.origin[1])
+            return
 
-		self.width, self.height = self.height, self.width
-		self.advanceWidth, self.advanceHeight = self.advanceHeight, self.advanceWidth
+        if n == 1:
+            self.bitmap = [list(row) for row in zip(*self.bitmap)]
+            self.bitmap.reverse()
+            self.origin = (self.origin[1], self.width - self.origin[0])
+        if n == 3:
+            self.bitmap.reverse()
+            self.bitmap = [list(row) for row in zip(*self.bitmap)]
+            self.origin = (self.height - self.origin[1], self.origin[0])
 
-	def scale(self, x=1, y=1):
-		assert x >= 0 and y >= 0
+        self.width, self.height = self.height, self.width
+        self.advanceWidth, self.advanceHeight = self.advanceHeight, self.advanceWidth
 
-		self._scaley(y)
+    def scale(self, x=1, y=1):
+        assert x >= 0 and y >= 0
 
-		self.bitmap = [list(row) for row in zip(*self.bitmap)]
-		self.width, self.height = self.height, self.width
-		self._scaley(x)
-		self.bitmap = [list(row) for row in zip(*self.bitmap)]
-		self.width, self.height = self.height, self.width
+        self._scaley(y)
 
-		self.advanceWidth *= x
-		self.advanceHeight *= y
-		self.voriginy *= y
-		self.origin = (self.origin[0] * x, self.origin[1] * y)
+        self.bitmap = [list(row) for row in zip(*self.bitmap)]
+        self.width, self.height = self.height, self.width
+        self._scaley(x)
+        self.bitmap = [list(row) for row in zip(*self.bitmap)]
+        self.width, self.height = self.height, self.width
 
-	def _scaley(self, sy):
-		if sy == 1:
-			return
+        self.advanceWidth *= x
+        self.advanceHeight *= y
+        self.voriginy *= y
+        self.origin = (self.origin[0] * x, self.origin[1] * y)
 
-		newbmp = []
+    def _scaley(self, sy):
+        if sy == 1:
+            return
 
-		for y in range(len(self.bitmap)):
-			for i in range(len(newbmp), int((y + 1) * sy)):
-				newbmp.append(list(self.bitmap[y]))
+        newbmp = []
 
-		self.bitmap = newbmp
-		self.height = len(newbmp)
+        for y in range(len(self.bitmap)):
+            for i in range(len(newbmp), int((y + 1) * sy)):
+                newbmp.append(list(self.bitmap[y]))
 
-	def dotiter(self):
-		for y in range(self.height):
-			for x in range(self.width):
-				if self.bitmap[y][x]:
-					yield (x - self.origin[0], y - self.origin[1])
+        self.bitmap = newbmp
+        self.height = len(newbmp)
 
-	def getBoundingBox(self):
-		INFINITY = float("inf")
-		minX = +INFINITY
-		minY = +INFINITY
-		maxX = -INFINITY
-		maxY = -INFINITY
-		for x, y in self.dotiter():
-			minX = min(minX, x)
-			minY = min(minY, y)
-			maxX = max(maxX, x)
-			maxY = max(maxY, y)
+    def dotiter(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.bitmap[y][x]:
+                    yield (x - self.origin[0], y - self.origin[1])
 
-		if minX == +INFINITY:
-			return (0, 0, 0, 0)
+    def getBoundingBox(self):
+        INFINITY = float("inf")
+        minX = +INFINITY
+        minY = +INFINITY
+        maxX = -INFINITY
+        maxY = -INFINITY
+        for x, y in self.dotiter():
+            minX = min(minX, x)
+            minY = min(minY, y)
+            maxX = max(maxX, x)
+            maxY = max(maxY, y)
 
-		return (minX, minY, maxX, maxY)
+        if minX == +INFINITY:
+            return (0, 0, 0, 0)
 
-	def getPixel(self, x, y):
-		x += self.origin[0]
-		y += self.origin[1]
-		if not (0 <= x < self.width and 0 <= y < self.height):
-			return False
-		return self.bitmap[int(round(y))][int(round(x))]
+        return (minX, minY, maxX, maxY)
 
-	def getPixel2(self, x, y):
-		if not (0 <= x < self.width and 0 <= y < self.height):
-			return False
-		return self.bitmap[y][x]
+    def getPixel(self, x, y):
+        x += self.origin[0]
+        y += self.origin[1]
+        if not (0 <= x < self.width and 0 <= y < self.height):
+            return False
+        return self.bitmap[int(round(y))][int(round(x))]
 
-	def toPolygons(self):
-		vertices = []
+    def getPixel2(self, x, y):
+        if not (0 <= x < self.width and 0 <= y < self.height):
+            return False
+        return self.bitmap[y][x]
 
-		for y in range(self.height + 1):
-			for x in range(self.width + 1):
-				b0 = bool(self.getPixel2(x - 1, y - 1))
-				b1 = bool(self.getPixel2(x,     y - 1))
-				b2 = bool(self.getPixel2(x - 1, y))
-				b3 = bool(self.getPixel2(x,     y))
-				edgedirs = _polygons_bit2edgedirs[(b0, b1, b2, b3)]
-				vertices.extend([[x - self.origin[0], y - self.origin[1], dir_in, dir_out] for dir_in, dir_out in edgedirs])
+    def toPolygons(self):
+        vertices = []
 
-		vertices_by_dir_in = {
-			_D: [],
-			_L: [],
-			_R: [],
-			_U: []
-		}
-		for v in vertices:
-			vertices_by_dir_in[v[2]].append(v)
+        for y in range(self.height + 1):
+            for x in range(self.width + 1):
+                b0 = bool(self.getPixel2(x - 1, y - 1))
+                b1 = bool(self.getPixel2(x,     y - 1))
+                b2 = bool(self.getPixel2(x - 1, y))
+                b3 = bool(self.getPixel2(x,     y))
+                edgedirs = _polygons_bit2edgedirs[(b0, b1, b2, b3)]
+                vertices.extend([[x - self.origin[0], y - self.origin[1],
+                                  dir_in, dir_out] for dir_in, dir_out in edgedirs])
 
-		polygons = []
-		while vertices:
-			polygon = []
-			v0 = vertices[0]
-			v = v0
-			while True:
-				polygon.append(v[0:2])
+        vertices_by_dir_in = {
+            _D: [],
+            _L: [],
+            _R: [],
+            _U: []
+        }
+        for v in vertices:
+            vertices_by_dir_in[v[2]].append(v)
 
-				next_dir_in = 3 - v[3]
-				if   next_dir_in == _D:
-					v1 = sorted([vn for vn in vertices_by_dir_in[next_dir_in] if vn[0] == v[0] and vn[1] > v[1]], key=lambda vn: vn[1] - v[1])[0]
-				elif next_dir_in == _U:
-					v1 = sorted([vn for vn in vertices_by_dir_in[next_dir_in] if vn[0] == v[0] and vn[1] < v[1]], key=lambda vn: v[1] - vn[1])[0]
-				elif next_dir_in == _L:
-					v1 = sorted([vn for vn in vertices_by_dir_in[next_dir_in] if vn[1] == v[1] and vn[0] > v[0]], key=lambda vn: vn[0] - v[0])[0]
-				elif next_dir_in == _R:
-					v1 = sorted([vn for vn in vertices_by_dir_in[next_dir_in] if vn[1] == v[1] and vn[0] < v[0]], key=lambda vn: v[0] - vn[0])[0]
-				vertices.remove(v1)
-				vertices_by_dir_in[v1[2]].remove(v1)
-				if v1 is v0:
-					break
-				v = v1
-			polygons.append(polygon)
+        polygons = []
+        while vertices:
+            polygon = []
+            v0 = vertices[0]
+            v = v0
+            while True:
+                polygon.append(v[0:2])
 
-		return polygons
+                next_dir_in = 3 - v[3]
+                if next_dir_in == _D:
+                    v1 = sorted([vn for vn in vertices_by_dir_in[next_dir_in] if vn[0]
+                                 == v[0] and vn[1] > v[1]], key=lambda vn: vn[1] - v[1])[0]
+                elif next_dir_in == _U:
+                    v1 = sorted([vn for vn in vertices_by_dir_in[next_dir_in] if vn[0]
+                                 == v[0] and vn[1] < v[1]], key=lambda vn: v[1] - vn[1])[0]
+                elif next_dir_in == _L:
+                    v1 = sorted([vn for vn in vertices_by_dir_in[next_dir_in] if vn[1]
+                                 == v[1] and vn[0] > v[0]], key=lambda vn: vn[0] - v[0])[0]
+                elif next_dir_in == _R:
+                    v1 = sorted([vn for vn in vertices_by_dir_in[next_dir_in] if vn[1]
+                                 == v[1] and vn[0] < v[0]], key=lambda vn: v[0] - vn[0])[0]
+                vertices.remove(v1)
+                vertices_by_dir_in[v1[2]].remove(v1)
+                if v1 is v0:
+                    break
+                v = v1
+            polygons.append(polygon)
 
-	def getImageDataSize(self):
-		bits = self.width * self.height
-		return (bits + 7) // 8
+        return polygons
 
-	def toImageData(self):
-		buf = reduce(lambda a, b: a + b, reversed(self.bitmap), [])
-		return _binary2imagedata(buf)
+    def getImageDataSize(self):
+        bits = self.width * self.height
+        return (bits + 7) // 8
 
-	def hasSameMetrics(self, other):
-		return all(getattr(self, key) == getattr(other, key) for key in ("width", "height", "origin", "advanceWidth", "advanceHeight", "voriginy"))
+    def toImageData(self):
+        buf = reduce(lambda a, b: a + b, reversed(self.bitmap), [])
+        return _binary2imagedata(buf)
 
-	def __str__(self):
-		return "O({0[0]},{0[1]}), aw={1}, ah={2}, vo={3}\n".format(self.origin, self.advanceWidth, self.advanceHeight, self.voriginy) + "\n".join("".join("@" if c else "." for c in r) for r in reversed(self.bitmap))
+    def hasSameMetrics(self, other):
+        return all(getattr(self, key) == getattr(other, key) for key in ("width", "height", "origin", "advanceWidth", "advanceHeight", "voriginy"))
+
+    def __str__(self):
+        return "O({0[0]},{0[1]}), aw={1}, ah={2}, vo={3}\n".format(self.origin, self.advanceWidth, self.advanceHeight, self.voriginy) + "\n".join("".join("@" if c else "." for c in r) for r in reversed(self.bitmap))
--- bitmap2otf/dotshape.py
+++ bitmap2otf/dotshape.py
@@ -5,156 +5,158 @@ from xml.etree import ElementTree
 
 
 def _intorfloat(v):
-	vi = int(v)
-	if v == vi:
-		return vi
-	return v
+    vi = int(v)
+    if v == vi:
+        return vi
+    return v
 
 
 def _vec2string(x, y, suf=""):
-	if x == 0:
-		return "{} v{}".format(_intorfloat(y), suf)
-	if y == 0:
-		return "{} h{}".format(_intorfloat(x), suf)
-	return "{} {} r{}".format(_intorfloat(x), _intorfloat(y), suf)
+    if x == 0:
+        return "{} v{}".format(_intorfloat(y), suf)
+    if y == 0:
+        return "{} h{}".format(_intorfloat(x), suf)
+    return "{} {} r{}".format(_intorfloat(x), _intorfloat(y), suf)
 
 
 def _evalxy(xystr, x=1.0, y=1.0):
-	if xystr[-1] == "x":
-		return float(xystr[:-1]) * x
-	if xystr[-1] == "y":
-		return float(xystr[:-1]) * y
-	return float(xystr)
+    if xystr[-1] == "x":
+        return float(xystr[:-1]) * x
+    if xystr[-1] == "y":
+        return float(xystr[:-1]) * y
+    return float(xystr)
 
 
 class DotShape(object):
-	def getGlyphBBX(self, bitmap, dw=100.0, dh=100.0):
-		bBBX = bitmap.getBoundingBox()
-		dBBX = self.getDotBBX(dw, dh)
-		return [
-			bBBX[0] * dw + dBBX[0],
-			bBBX[1] * dh + dBBX[1],
-			bBBX[2] * dw + dBBX[2],
-			bBBX[3] * dh + dBBX[3]
-		]
+    def getGlyphBBX(self, bitmap, dw=100.0, dh=100.0):
+        bBBX = bitmap.getBoundingBox()
+        dBBX = self.getDotBBX(dw, dh)
+        return [
+            bBBX[0] * dw + dBBX[0],
+            bBBX[1] * dh + dBBX[1],
+            bBBX[2] * dw + dBBX[2],
+            bBBX[3] * dh + dBBX[3]
+        ]
 
 
 class DotShapePixelOutline(DotShape):
-	def bitmap2charstring(self, bitmap, dw=100.0, dh=100.0, subrs=[]):
-		polygons = bitmap.toPolygons()
-		if not polygons:
-			return ""
+    def bitmap2charstring(self, bitmap, dw=100.0, dh=100.0, subrs=[]):
+        polygons = bitmap.toPolygons()
+        if not polygons:
+            return ""
 
-		buf = ""
-		x = y = 0.0
-		for polygon in polygons:
-			x1, y1 = polygon[0]
-			buf += _vec2string((x1 - x) * dw, (y1 - y) * dh, "moveto ")
-			x, y = x1, y1
+        buf = ""
+        x = y = 0.0
+        for polygon in polygons:
+            x1, y1 = polygon[0]
+            buf += _vec2string((x1 - x) * dw, (y1 - y) * dh, "moveto ")
+            x, y = x1, y1
 
-			if polygon[0][0] == polygon[1][0]:
-				currentDirection = 0
-			elif polygon[0][1] == polygon[1][1]:
-				currentDirection = 1
-			else:
-				assert False
+            if polygon[0][0] == polygon[1][0]:
+                currentDirection = 0
+            elif polygon[0][1] == polygon[1][1]:
+                currentDirection = 1
+            else:
+                assert False
 
-			ops = ["hlineto ", "vlineto "]
-			op = ops[1 - currentDirection]
-			args = 0
+            ops = ["hlineto ", "vlineto "]
+            op = ops[1 - currentDirection]
+            args = 0
 
-			for (x1, y1), (x, y) in zip(polygon[:-1], polygon[1:]):
-				distance = ((x - x1) * dw, (y - y1) * dh)
-				assert distance[currentDirection] == 0.0
-				currentDirection = 1 - currentDirection
-				buf += "{} ".format(_intorfloat(distance[currentDirection]))
+            for (x1, y1), (x, y) in zip(polygon[:-1], polygon[1:]):
+                distance = ((x - x1) * dw, (y - y1) * dh)
+                assert distance[currentDirection] == 0.0
+                currentDirection = 1 - currentDirection
+                buf += "{} ".format(_intorfloat(distance[currentDirection]))
 
-				args += 1
-				# Type 2 charstring interpreter's argument stack has limit of 48
-				if args == 48:
-					buf += op
-					op = ops[1 - currentDirection]
-					args = 0
+                args += 1
+                # Type 2 charstring interpreter's argument stack has limit of 48
+                if args == 48:
+                    buf += op
+                    op = ops[1 - currentDirection]
+                    args = 0
 
-			if args == 0:
-				pass
-			else:
-				buf += op
+            if args == 0:
+                pass
+            else:
+                buf += op
 
-		return buf.strip()
+        return buf.strip()
 
-	def getSubroutines(self, dw=100.0, dh=100.0):
-		return []
+    def getSubroutines(self, dw=100.0, dh=100.0):
+        return []
 
-	def getDotBBX(self, dw=100.0, dh=100.0):
-		return [0.0, 0.0, dw, dh]
+    def getDotBBX(self, dw=100.0, dh=100.0):
+        return [0.0, 0.0, dw, dh]
 
 
 _FACTOR_XORY_RE = re.compile(r"^[+-]?[\d.]+(?:e[+-]?\d+)?[xy]$")
 
 
 class DotShapeExternal(DotShape):
-	def __init__(self, src, sx=1.0, sy=1.0):
-		shape = ElementTree.parse(src).getroot()
-		startX = shape.get("startX", "0x")
-		startY = shape.get("startY", "0y")
-		self.startX = startX
-		self.startY = startY
-		self.endX = shape.get("endX", startX)
-		self.endY = shape.get("endY", startY)
-		self.charstring = shape.text
-		self.sx = sx
-		self.sy = sy
-		self.bbx = [
-			shape.get("minX", "0x"),
-			shape.get("minY", "0y"),
-			shape.get("maxX", "1x"),
-			shape.get("maxY", "1y")
-		]
+    def __init__(self, src, sx=1.0, sy=1.0):
+        shape = ElementTree.parse(src).getroot()
+        startX = shape.get("startX", "0x")
+        startY = shape.get("startY", "0y")
+        self.startX = startX
+        self.startY = startY
+        self.endX = shape.get("endX", startX)
+        self.endY = shape.get("endY", startY)
+        self.charstring = shape.text
+        self.sx = sx
+        self.sy = sy
+        self.bbx = [
+            shape.get("minX", "0x"),
+            shape.get("minY", "0y"),
+            shape.get("maxX", "1x"),
+            shape.get("maxY", "1y")
+        ]
 
-	def bitmap2charstring(self, bitmap, dw=100.0, dh=100.0, subrs=[]):
-		dots = list(bitmap.dotiter())
-		if not dots:
-			return ""
+    def bitmap2charstring(self, bitmap, dw=100.0, dh=100.0, subrs=[]):
+        dots = list(bitmap.dotiter())
+        if not dots:
+            return ""
 
-		sx = self.sx
-		sy = self.sy
-		sw = sx * dw
-		sh = sy * dh
-		startX = _evalxy(self.startX, x=sw, y=sh)
-		startY = _evalxy(self.startY, x=sw, y=sh)
-		endX   = _evalxy(self.endX,   x=sw, y=sh)
-		endY   = _evalxy(self.endY,   x=sw, y=sh)
+        sx = self.sx
+        sy = self.sy
+        sw = sx * dw
+        sh = sy * dh
+        startX = _evalxy(self.startX, x=sw, y=sh)
+        startY = _evalxy(self.startY, x=sw, y=sh)
+        endX = _evalxy(self.endX,   x=sw, y=sh)
+        endY = _evalxy(self.endY,   x=sw, y=sh)
 
-		subrno = subrs[0]
-		e2sX = startX - endX
-		e2sY = startY - endY
+        subrno = subrs[0]
+        e2sX = startX - endX
+        e2sY = startY - endY
 
-		buf = ""
+        buf = ""
 
-		buf += _vec2string(dots[0][0] * dw + startX, dots[0][1] * dh + startY, "moveto ")
-		buf += "{} callsubr ".format(subrno)
+        buf += _vec2string(dots[0][0] * dw + startX,
+                           dots[0][1] * dh + startY, "moveto ")
+        buf += "{} callsubr ".format(subrno)
 
-		for (x0, y0), (x1, y1) in zip(dots[:-1], dots[1:]):
-			buf += _vec2string((x1 - x0) * dw + e2sX, (y1 - y0) * dh + e2sY, "moveto ")
-			buf += "{} callsubr ".format(subrno)
+        for (x0, y0), (x1, y1) in zip(dots[:-1], dots[1:]):
+            buf += _vec2string((x1 - x0) * dw + e2sX,
+                               (y1 - y0) * dh + e2sY, "moveto ")
+            buf += "{} callsubr ".format(subrno)
 
-		return buf.strip()
+        return buf.strip()
 
-	def getSubroutines(self, dw=100.0, dh=100.0):
-		sw = self.sx * dw
-		sh = self.sy * dh
+    def getSubroutines(self, dw=100.0, dh=100.0):
+        sw = self.sx * dw
+        sh = self.sy * dh
 
-		buf = []
-		for token in self.charstring.split():
-			if _FACTOR_XORY_RE.match(token):
-				buf.append(str(_evalxy(token, x=sw, y=sh)))
-			else:
-				buf.append(token)
+        buf = []
+        for token in self.charstring.split():
+            if _FACTOR_XORY_RE.match(token):
+                buf.append(str(_evalxy(token, x=sw, y=sh)))
+            else:
+                buf.append(token)
 
-		return [" ".join(buf)]
+        return [" ".join(buf)]
 
-	def getDotBBX(self, dw=100.0, dh=100.0):
-		sw = self.sx * dw
-		sh = self.sy * dh
-		return [_evalxy(v, x=sw, y=sh) for v in self.bbx]
+    def getDotBBX(self, dw=100.0, dh=100.0):
+        sw = self.sx * dw
+        sh = self.sy * dh
+        return [_evalxy(v, x=sw, y=sh) for v in self.bbx]
